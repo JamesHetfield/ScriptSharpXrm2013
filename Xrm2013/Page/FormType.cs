a43170c1746af26b42f7c420b4a957dab620d4a6
James Hetfield <fbz.hetfield@gmail.com>
2013-11-14 13:56:02 +0000

    Obsolete functions
    Re-added the obsolete functions to preserve backward compatibility with
    Dynamics CRM 2011 (pre-UR8) and above.  Added Obsolete messages to
    functions that have been depreciated.

--- Xrm2013/Page/FormType.cs
+++ Xrm2013/Page/FormType.cs
@@ -7,7 +7,6 @@ namespace Xrm
     /// <summary>
     /// Represents a FormType enumeration.
     /// </summary>
-    /// <remarks>I have removed the QuickCreate (5) and ReadOptimized (11) options as they are depreciated</remarks>
     [IgnoreNamespace]
     [Imported]
     [NumericValues]
@@ -18,7 +17,11 @@ namespace Xrm
         Update = 2,
         ReadOnly = 3,
         Disabled = 4,
+        [Obsolete("Deprecated as of CRM 2011", false)]
+        QuickCreate = 5,
         BulkEdit = 6,
+        [Obsolete("Deprecated as of CRM 2013", false)]
+        ReadOptimized = 11
     }
 
 }
